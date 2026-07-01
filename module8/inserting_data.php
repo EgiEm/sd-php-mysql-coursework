750f5d95517920157171e5ed3b90ffdca3921866
EgiEm <egiem@users.noreply.github.com>
2026-07-01 11:00:48 +0200

    Add backend PHP controller page script

---
+++ module8/inserting_data.php
@@ -0,0 +1,23 @@
+<?php
+$host = "localhost";
+$db = "db";
+$user = "root";
+$pass = "";
+
+try{
+    $pdo = new PDO ("mysql:host=$host;dbname=$db", $user, $pass);
+
+    $username = "Endri";
+
+    $password = password_hash("mypassword", PASSWORD_DEFAULT);
+
+    $sql = "INSERT INTO users(username, password) VALUES ('$username', '$password')";
+
+    $pdo -> exec($sql);
+
+    echo "Bravo";
+}catch(PDOException $e){
+    echo $e->getMessage();
+}
+
+?>
