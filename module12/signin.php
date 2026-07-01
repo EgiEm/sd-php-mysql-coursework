e231fc7484725663cb5e166b946f7fbae66c70c2
EgiEm <egiem@users.noreply.github.com>
2026-07-01 11:00:46 +0200

    Add backend PHP controller page script

---
+++ module12/signin.php
@@ -0,0 +1,27 @@
+<?php include "header.php"; ?>
+<form>
+<div class="form-group">
+    <label for="exampleInputName">Name</label>
+    <input type="username" class="form-control" id="exampleInputUsername" placeholder="Name">
+    
+  </div>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Lastname</label>
+    <input type="username" class="form-control" id="exampleInputUsermane"  placeholder="Lastname">
+    
+  </div>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Username</label>
+    <input type="username" class="form-control" id="exampleInputUsermane"  placeholder="Username">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Email address</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+  <div class="form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+<?php include "footer.php"; ?>
